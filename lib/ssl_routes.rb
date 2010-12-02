eed6c0f36d92e49943393606986e5d7f9ada6023
Cedric Howe <cedric@kickstarter.com>
2010-12-02 17:42:49 -0500

    Removed some comments and ruby-debug.

--- lib/ssl_routes.rb
+++ lib/ssl_routes.rb
@@ -1,21 +1,5 @@
 module SslRoutes
   
-  # Features:
-  # - [DONE] enable/disable ssl
-  # - [DONE] enforce protocol in controller
-  # - [DONE] route parameter option
-  # - [DONE] secure session (firesheep)
-  # - [DONE] fix urls (paperclip)
-  # - [DONE] fix urls (url_for - controller and view)
-  # - presentation plugin
-  #
-  # Other:
-  # - cross protocol form submits
-  # - question: canonical urls?
-  # - question: fractured pagerank?
-  
-  require 'ruby-debug'
-  
   require 'ssl_routes/controller'
   require 'ssl_routes/paperclip' if defined?( Paperclip )
   
