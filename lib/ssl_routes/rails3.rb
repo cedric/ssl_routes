514c6f8c24490efcfc1c6fc5d7fb2baf39c63bcd
Lance Ivy <lance@cainlevy.net>
2011-09-23 16:01:14 -0700

    RouteSet#recognize_routes expects an environment hash with only a :method key.

--- lib/ssl_routes/rails3.rb
+++ lib/ssl_routes/rails3.rb
@@ -39,7 +39,7 @@ module SslRoutes
 
         def ensure_protocol
           routes = Rails.application.routes
-          options = routes.recognize_path request.path, request.env
+          options = routes.recognize_path request.path, {:method => request.env['REQUEST_METHOD']}
           current, target = determine_protocols(options)
           if current != target && !request.xhr? && request.get?
             flash.keep
