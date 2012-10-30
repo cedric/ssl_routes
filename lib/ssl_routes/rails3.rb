06845195b0faded52a64d1a518f19f8980544c32
Tieg Zaharia <tieg@kickstarter.com>
2012-10-30 18:23:52 -0400

    replace URI parsing w/str interpolation to avoid unicode errs
    Conflicts:
    	lib/ssl_routes/rails3.rb

--- lib/ssl_routes/rails3.rb
+++ lib/ssl_routes/rails3.rb
@@ -43,7 +43,7 @@ module SslRoutes
           current, target = determine_protocols(options)
           if current != target && !request.xhr? && request.get?
             flash.keep
-            redirect_to URI.join("#{target}://#{request.host_with_port}", request.fullpath).to_s
+            redirect_to "#{target}://#{request.host_with_port}#{request.fullpath}"
             return false
           end
         end
