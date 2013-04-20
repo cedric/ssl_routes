ba106dc7df34fb1166c14094d4263e9a2b71481c
George Mendoza <gsmendoza@gmail.com>
2013-04-20 14:04:40 +0800

    Add Rails 3.2.13 dummy application for testing gem.

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'bf5231942ef8aa899beeb58e41a177b2b8e09c5260f4180eff81863cebe2aba09e13d4356c89f0faef420d290fe4016952a52ac0079559aa86a120708b4ae931'
